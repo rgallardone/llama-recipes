4d275780f42e738306a4e3e59c0d9610bb9ee51b
Rodrigo <rgallardo@tryolabs.com>
2023-10-13 12:56:22 +0000

    Adjust execution scripts for the cluster

--- train_and_eval.sh
+++ train_and_eval.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if arguments are passed
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <dataset> <is_UD> <task>"
+    exit 1
+fi
+
 # Get the current date and time
 current_date_time=$(date '+%Y-%m-%d_%H-%M-%S')
 
@@ -8,11 +14,11 @@ dir_name="experiment_${current_date_time}"
 mkdir ${dir_name}
 
 # Run the training
-training_args="--nnodes 1 --nproc_per_node 2 --rdzv-backend c10d --rdzv-endpoint localhost:0 llama_finetuning.py --enable_fsdp --use_peft --peft_method lora --model_name /dataslow/toti/llama-2/Llama-2-7b-chat-hf --pure_bf16 --dataset ancora_co_es --num_epochs 1 --save_model --dist_checkpoint_root_folder model_checkpoints --dist_checkpoint_folder fine-tuned --output_dir ${dir_name}"
+training_args="--nnodes 1 --nproc_per_node 2 --rdzv-backend c10d --rdzv-endpoint localhost:0 llama_finetuning.py --enable_fsdp --use_peft --peft_method lora --model_name /dataslow/toti/llama-2/Llama-2-7b-chat-hf --pure_bf16 --dataset $1 --num_epochs 1 --save_model --dist_checkpoint_root_folder model_checkpoints --dist_checkpoint_folder fine-tuned --output_dir ${dir_name}"
 torchrun ${training_args} > ${dir_name}/training_output.txt
 
 # Run the evaluation
-eval_args="--eval_dir ${dir_name} --model_name /dataslow/toti/llama-2/Llama-2-7b-chat-hf --peft_model_name ${dir_name} --max_new_tokens 500 --max_sentences_per_doc 10 --top_p 0.9 --top_k 30 --do_sample True --temperature 0.25"
+eval_args="--eval_dir ${dir_name} --model_name /dataslow/toti/llama-2/Llama-2-7b-chat-hf --peft_model_name ${dir_name} --max_new_tokens 500 --max_sentences_per_doc 10 --top_p 0.9 --top_k 30 --do_sample True --temperature 0.25 --is_UD $2 --task $3"
 python coref_eval.py ${eval_args} > ${dir_name}/eval_output.txt
 
 # Store the arguments
